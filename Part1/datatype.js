50b1aabf95db301cd98108596c1bf2d47bc3118a
Kunalsk36 <kunalkavathekar@gmail.com>
2026-01-04 19:23:06 +0530

    learned about datatypes

---
+++ Part1/datatype.js
@@ -0,0 +1,39 @@
+/*
+
+Following are the datatypes available in JavaScript:
+
+String
+Number
+Boolean
+Bigint
+
+Undefined
+null
+
+Object
+
+Symbol
+
+
+*/
+
+// Examples:
+var score = 100; // this is an old way
+
+let marks = 99; // number
+let username = "Kunal" // string
+let isLoggedin = false; // boolean
+
+// object
+let drinksTypes = ["Coffee", "Tea", "Juice"];
+let user = {firstName: "Kunal", lastName: "Kavathekar"};
+
+console.log(score);
+
+console.log(marks);
+console.log(username);
+console.log(isLoggedin);
+
+console.log(drinksTypes);
+console.log(user);
+
