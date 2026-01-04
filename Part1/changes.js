817c899032ea0faaa46f863fcb03abe4204756d4
Kunalsk36 <kunalkavathekar@gmail.com>
2026-01-04 19:30:05 +0530

    learned about const variable

---
+++ Part1/changes.js
@@ -0,0 +1,16 @@
+// const keyword is used to create a constant variable.
+// it can not change its value once it defined.
+
+// example
+const username = "kunalsk36";
+console.log(username);
+
+// if we try to change it will throw error
+// username = "newUserName";
+// console.error(username); if we run these these will throw an error
+
+// we can change values of variables which is defined by var or let.
+let myFriendName = "Kunal";
+console.log(myFriendName);
+myFriendName = "Tanvi";
+console.log(myFriendName);
